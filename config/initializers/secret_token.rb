a8f6c1f1ce7a37950ca292f2c6cbee4d62426a5a
antoshalee <antoshalee@gmail.com>
2013-04-13 22:58:50 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shlina::Application.config.secret_token = 'dcd60334a6a420003b3ee290916b335f7a1a3bd3533d522d007afd35fb14caac23f7b0c34baceb4bd3b4e3114774ea1e49dbd6a0a0114df31e9a3d32671659b8'
